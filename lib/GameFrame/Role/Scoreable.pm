50e9dc7879c2bccf91f4fa3599618b15bae7eb44
eilara <ran.eilam@gmail.com>
2011-03-31 01:14:44 +0200

    22-kill-the-circles: added score

---
+++ lib/GameFrame/Role/Scoreable.pm
@@ -0,0 +1,32 @@
+package GameFrame::Role::Scoreable;
+
+# shows a score counter and keeps score
+
+use Moose::Role;
+use MooseX::Types::Moose qw(Num ArrayRef);
+
+with 'GameFrame::Role::Paintable';
+
+has score => (
+    is       => 'rw',
+    isa      => Num,
+    default  => 0,
+);
+
+has score_xy => (
+    is       => 'ro',
+    isa      => ArrayRef[Num],
+    default  => sub { [600, 20] },
+);
+
+sub add_to_score {
+    my ($self, $add) = @_;
+    $self->score( $self->score + $add );
+}
+
+after paint => sub {
+    my ($self, $surface) = @_;
+    $surface->draw_gfx_text($self->score_xy, 0xFFFF00FF, $self->score);
+};
+
+1;
